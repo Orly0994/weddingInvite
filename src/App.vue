2ac39f800dc58becefe55c9a53f50e7a3239caaf
Andrey Sokolov <a.sokolov@iblschool.ru>
2024-10-13 14:57:18 +0300

    changed configs

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div>
+    <HelloScreen />
     <div>{{ message }}</div>
+    <div>{{ staticMessage }}</div>
 
     <div>
       <button @click="onClickBtn">click me!</button>
+      <button @click="onClickBtnStaticMessage">click me 2!</button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, Ref } from 'vue'
+import HelloScreen from './components/HelloScreen.vue'
 
 const message: Ref<string> = ref('Hello there')
 
+let staticMessage: string = 'static message'
+
 const onClickBtn = () => {
   message.value = 'changed message'
+}
+
+const onClickBtnStaticMessage = () => {
+  staticMessage = 'static 2 message'
 }
 </script>
---
+++ src/components/HelloScreen.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    HelloScreen
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
